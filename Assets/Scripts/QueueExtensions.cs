a8bafce491f43149e9bfbe699e029d61ab921911
박서진 <kevin8193@naver.com>
2025-11-13 18:39:51 +0900

    [FEAT] Queue Extension Methods

---
+++ Assets/Scripts/QueueExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class QueueExtensions
+{
+    public static int GetCount<T>(this Queue<T> queue, T itemToCount)
+    {
+        var comparer = EqualityComparer<T>.Default;
+        return queue.Count(item => comparer.Equals(item, itemToCount));
+    }
+    
+    
+    // if queue is full, pop one element and enqueue the target value;
+    public static void CapacitySafeEnqueue<T>(this Queue<T> queue, T value, int capacity)
+    {
+        if (queue.Count == capacity) queue.Dequeue();
+            
+        queue.Enqueue(value);
+    }
+    
+}
